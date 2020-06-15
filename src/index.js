459c8f75fc4a269c0e668fb2a78ce4e893dc3995
María Pérez <maria_erez@hotmail.es>
2020-06-15 11:29:39 -0500

    Second problem: resolved

--- src/index.js
+++ src/index.js
@@ -5,11 +5,23 @@ const randomTime=(maximo, minimo)=>Math.round(Math.random()*(maximo-minimo)+mini
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
-    if(randomTime(maximo,minimo)<=time){
+    let Timing=randomTime(maximo, minimo);
+    if(Timing<=time){
       setTimeout(() => {
-        resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
+        resolve(`=== Pedido servido: ${product}, tiempo de preparación ${Timing}ms para la ${table}`);
       }, time);
-    }else reject(`La orden fue entregada fuera del tiempo establecido`)
+    }else reject(`La orden ${product} para ${table} fue entregada fuera del tiempo establecido a ${Timing}ms`);
+  });
+}
+
+const pickup_orders = (time, product, table) => {
+  return new Promise((resolve, reject) => {
+    let Timing=randomTime(maximo, minimo);
+    if(Timing<=time){
+      setTimeout(() => {
+        resolve(`=== Pedido retirado: ${product}, tiempo de retiro ${Timing}ms de la ${table}`);
+      }, time);
+    }else reject(`El pedido ${product} para la ${table} fue recogido fuera del rango de tiempo requerido a ${Timing}ms`);
   });
 }
 
@@ -27,4 +39,15 @@ const waiter = () => {
     .catch((err) => console.error(err));
 };
 
+const waiter2=()=>{
+  pickup_orders(6000, menu.hotdog, table[0])
+    .then((res) =>{
+      console.log(res)
+      return pickup_orders(6000, menu.pizza,table[2])
+    })
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+}
+
 waiter();
+waiter2();
