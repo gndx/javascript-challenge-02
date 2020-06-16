5f41dbe6a923c5a777fed1d84731e1386054d0e7
María Pérez <maria_erez@hotmail.es>
2020-06-15 21:56:05 -0500

    lacking

--- src/index.js
+++ src/index.js
@@ -49,5 +49,32 @@ const waiter2=()=>{
     .catch((err) => console.error(err));
 }
 
+const onerror=()=>console.log(`No se puedo retirar el pedido`);
+
+const pickup_order2 = (time, product, table) => {
+  return new Promise((resolve, reject) => {
+    let Timing=randomTime(maximo, minimo);
+    if(Timing<=time){
+      setTimeout(() => {
+        resolve(`=== Pedido retirado: ${product}, tiempo de retiro ${Timing}ms de la ${table}`);
+      }, time);
+    }else reject(`El pedido ${product} para la ${table} fue recogido fuera del rango de tiempo requerido a ${Timing}ms`);
+  });
+}
+
+const waiter3=()=>{
+  async const pickup_order2=()=>{
+    const order=[(6000, menu.hotdog, table[1]),(6000, menu.pizza, table[0]), (6000, menu.hotdog, table[1])];
+    const promises=order.map((time, product, table)=>pickup_order2(time, product, table));
+    try{
+      const pickup=await Promise.all(promises);
+      console.log(pickup);
+    }catch([time, product, table]){
+      onerror()
+    }
+  }
+}
+
 waiter();
 waiter2();
+waiter3();
