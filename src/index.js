c90fd59be2d3e8af8368387a6dafbe5122355e23
zichis77 <zichis33@hotmail.com>
2020-07-06 18:27:06 -0500

    Development Challenge 2
    Point 1 was developed by creating the "randomTime" function in milliseconds and placing it in all the functions.
    Point 2 and 3 were developed creating the functions "waiter2" with interlaced promises and "waiter3" with async / await.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+var min = 1000;
+var max = 8000;
+
+function randomTime (min, max) {
+   return Math.floor(Math.random() * (max - min)) + min;
+}
+var x = randomTime (min, max);
+randomTime(min, max);
+
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
     }, time);
-  });
+  }); fail(() => reject(time, product, table))
 }
 
 const menu = {
@@ -16,9 +25,40 @@ const menu = {
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
+  orders(x+3, menu.hamburger, table[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
 
 waiter();
+
+const waiter2 = () => {
+  orders(x, menu.hotdog, table[0])
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+};
+    orders(x+1, menu.pizza, table[2])
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+waiter2();
+
+const waiter3 = async () => {
+  try{
+     const orden= await orders();
+   orders(8005, menu.pizza, table[1])
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+
+  orders(8005, menu.hotdog, table[1])
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+
+    orders(8005, menu.hotdog, table[1])
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+    
+    } catch (err) {
+        console.log(err);
+      } 
+  }
+waiter3();
