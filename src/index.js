353ac9c00d361f063f4163d333a3fce233e02b59
Rony Antolinez <rony1710.dev@gmail.com>
2020-08-14 12:35:57 -0500

    challenge complete

--- src/index.js
+++ src/index.js
@@ -1,24 +1,63 @@
-const orders = (time, product, table) => {
-  console.log(`### Orden: ${product} para ${table}`);
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
-    }, time);
-  });
-}
-
 const menu = {
-  hamburger: 'Combo Hamburguesa',
+  hamburger: 'Burguer Combo',
   hotdog: 'Combo Hot Dogs',
   pizza: 'Combo Pizza',
 };
 
-const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
+const table = ['Table 1', 'Table 2', 'Table 3', 'Table 4', 'Table 5'];
+
+
+const randomTime = () => {
+  return Math.floor(Math.random() * 8000) + 1000;
+}
+
+const orders = (time, product, table) => {
+  console.log(`### Order: ${product} for ${table}`);
+  return new Promise((resolve, reject) => {
+    true
+
+      ? setTimeout(() => {
+        resolve(`=== Order Served: ${product}, prep time ${time}ms for the ${table}`);
+      }, time)
+
+      : reject('sorry we have a problem')
+
+  });
+}
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
+  orders(randomTime(), menu.hamburger, table[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
 
-waiter();
+
+const waiter2 = () =>  {
+  orders(randomTime(), menu.hotdog, table [0])
+    .then((res) => {
+      console.log(res)
+      return orders(randomTime(), menu.pizza, table [2])
+      .then((res) => {
+        console.log(res)
+      })
+    })
+    .catch((err) => console.error(err))
+}
+
+
+const waiter3 = async () => {
+  let orderTable2 = [menu.hamburger, menu.pizza, menu.hotdog]
+    try {
+      const ALL_ORDERS = orderTable2.map( order => orders(randomTime(), order, table[1]))
+      const allPromise = await Promise.all(ALL_ORDERS)
+      console.log(allPromise.toString())
+    }
+    catch (error) {
+      console.log(error)
+    }
+}
+
+
+waiter()
+waiter2()
+waiter3()
