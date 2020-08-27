1b739c0e03a3be2385311470777fea31cecea7eb
KuCuu <cucugutierrez1@gmail.com>
2020-08-26 21:52:04 -0500

    Solucion segundo problema

--- src/index.js
+++ src/index.js
@@ -32,5 +32,17 @@ const waiter = () => {
     .catch((err) => console.error(err));
 };
 
-waiter();
+const waiter2 = () => {
+  orders(randomTime(), menu.hotdog, table[0])
+  .then((res) => {
+    console.log(res);
+    return orders(randomTime(), menu.pizza, table[2])
+    })
+    .then((res) => {
+    console.log(res);
+    })      
+    .catch((err) => console.error(err));
+}
+//waiter();
+waiter2();
 //console.log(randomTime())
