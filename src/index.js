09278e80bacf21b74ac93c6f3ea9a4b6d05ce7ec
Angie Garcia <Angiegarciaoficial@gmail.com>
2020-09-30 18:11:48 -0500

    primer problema

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+const max = 8000
+const min = 1000
+
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
+      reject(`error`)
     }, time);
   });
 }
@@ -15,10 +19,19 @@ const menu = {
 
 const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
 
+
+const randonTime =(min, max) => {
+  return (Math.floor(Math.random()*(max-min + 1))+min)
+}
+
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
+  orders(randonTime(min, max), menu.hamburger, table[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
 
 waiter();
+
+
+
+  
