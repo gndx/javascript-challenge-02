c525f2ea3e87e2c80aac5a4ff9e5fe5a28aad6bf
Adriana Ardila <45320594+Ardilaadri@users.noreply.github.com>
2020-10-04 22:38:33 -0500

    All challenges complete
    I had some problems to upload all the files so I did it to a different way

--- src/index.js
+++ src/index.js
@@ -1,24 +1,55 @@
 const orders = (time, product, table) => {
   console.log(`### Orden: ${product} para ${table}`);
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(`=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`);
-    }, time);
+    if (time && product && table) {
+      setTimeout(() => {
+        resolve(
+          `=== Pedido servido: ${product}, tiempo de preparación ${time}ms para la ${table}`
+        );
+      }, time);
+    } else {
+      reject(`Tuvimos problemas con tu pedido`);
+    }
   });
-}
-
-const menu = {
-  hamburger: 'Combo Hamburguesa',
-  hotdog: 'Combo Hot Dogs',
-  pizza: 'Combo Pizza',
 };
 
-const table = ['Mesa 1', 'Mesa 2', 'Mesa 3', 'Mesa 4', 'Mesa 5'];
+const menu = {
+  hamburger: "Combo Hamburguesa",
+  hotdog: "Combo Hot Dogs",
+  pizza: "Combo Pizza",
+};
+
+const table = ["Mesa 1", "Mesa 2", "Mesa 3", "Mesa 4", "Mesa 5"];
+
+const randomTime = () => 1000 * ((Math.floor(Math.random() * 10) % 8) + 1);
 
 const waiter = () => {
-  orders(6000, menu.hamburger, table[3])
+  orders(randomTime(), menu.hamburger, table[3])
     .then((res) => console.log(res))
     .catch((err) => console.error(err));
 };
 
+const waiter2 = () => {
+  orders(randomTime(), menu.hotdog, table[0]).then((res) => console.log(res));
+  return orders(randomTime(), menu.pizza, table[2])
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+};
+
+async function waiter3() {
+  try {
+    const allOrders = [
+      await orders(randomTime(), menu.hotdog, table[1]),
+      await orders(randomTime(), menu.pizza, table[1]),
+      await orders(randomTime(), menu.hotdog, table[1]),
+    ];
+    const res_1 = await Promise.all(allOrders);
+    console.log(res_1);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 waiter();
+waiter2();
+waiter3();
